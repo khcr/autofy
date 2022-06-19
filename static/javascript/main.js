2d9cc66d67cb0a68c90d437187391ab68a96d909
antoine daeniker <antoine.daeniker@epfl.ch>
2022-06-19 13:53:01 +0200

    clean up and add some background imgs

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -4,6 +4,9 @@ playButton.onclick = () => {
     console.log(playButton.checked);
 };
 
+var weather_state = "sunny"
+var place_state = "countrySide"
+set_background(weather_state, place_state)
 // Background selection
 const background_button = document.querySelector('#backgrdbtn')
 background_button.onclick = (button) => {
@@ -11,7 +14,9 @@ background_button.onclick = (button) => {
     document.documentElement.style.setProperty('--light-pink', button.srcElement.dataset.value1);
     document.documentElement.style.setProperty('--deep-pink', button.srcElement.dataset.value2);
     if (button.srcElement.dataset.value3 != "None")
-        document.documentElement.style.setProperty('--background-image', "url('./" + button.srcElement.dataset.value3 + "')")
+        weather_state = button.srcElement.dataset.value3
+        set_background(weather_state, place_state)
+        //document.documentElement.style.setProperty('--background-image', "url('./lofi_background_" + button.srcElement.dataset.value3 + "')")
     document.getElementById("backButtonState").innerHTML = "<p>You are listening music with " + button.srcElement.value + " background"
 
     // update the music
@@ -26,8 +31,27 @@ background_button.onclick = (button) => {
 const places_button = document.querySelector('#placesbtn')
 places_button.onclick = (button) => {
     if (button.srcElement.dataset.value1 != null)
-    document.documentElement.style.setProperty('--background-image', "url('./" + button.srcElement.dataset.value1 + "')")
+        place_state = button.srcElement.dataset.value1
+        set_background(weather_state, place_state)
+    //document.documentElement.style.setProperty('--background-image', "url('./" + button.srcElement.dataset.value1 + "')")
     document.getElementById("placeButtonState").innerHTML = "<p>You are listening music in " + button.srcElement.value
+}
+
+function getURLName(weather, place) {
+    return "url('./lofi_background_" + weather + '_' + place + ".png')"
+}
+
+function set_background(weather, place) {
+    var url = null
+    if (place == "space") {
+        url = getURLName("", place)
+    } else if (place == "coffeeShop" && (weather == "thunder" || weather == "rain")) {
+        url = getURLName("night", place)
+    } else {
+        url = getURLName(weather, place)
+    }
+    console.log(url)
+    document.documentElement.style.setProperty('--background-image', url)
 }
 
 function osc_request(data) {
